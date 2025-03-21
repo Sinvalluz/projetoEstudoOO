66f14ac2490ae39c8340ce99f75a8614a8af1899
Sinval Luz <contato.sinvalluz@gmail.com>
2025-03-21 08:53:26 -0300

    feat: aprimora cálculo de salário e formatação de saída

--- src/main/java/roteiro1/parte2/Programa02.java
+++ src/main/java/roteiro1/parte2/Programa02.java
@@ -1,12 +1,17 @@
 package roteiro1.parte2;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Programa02 {
     public static void calcularSalario (double salarioBase, int horasExtras, int horasTrabalhadas) {
         double valorPorHora = salarioBase / horasTrabalhadas;
-        double salarioTotal = salarioBase + (valorPorHora * horasExtras);
+        double salarioTotal = salarioBase + valorPorHora * horasExtras;
 
-        System.out.println("Salário Base: R$ " + String.format("%.2f",salarioBase));
+
+        NumberFormat formatador = NumberFormat.getCurrencyInstance(Locale.of("pt", "BR"));
+
+        System.out.println("Salário Base: " + formatador.format(salarioBase));
         System.out.println("Quantidade de horas extras: " + horasExtras);
-        System.out.println("Salário total: R$ " + String.format("%.2f" , salarioTotal));
+        System.out.println("Salário total: " + formatador.format(salarioTotal));
     }
 }
